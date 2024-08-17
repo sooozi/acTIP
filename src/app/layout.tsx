f1f3ef6151324976e1a2f7d1e744fd88ffeb010c
Suzy Kim <skme1103@naver.com>
2024-08-17 19:57:57 +0900

    Add SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import MainMO from '../components/layout/MainMO';
 import MainPC from '../components/layout/MainPC';
 
 export const metadata: Metadata = {
-  title: 'Actip',
+  title: 'acTIP',
   icons: {
     icon: '/image/system/favicon.ico',
   },
@@ -19,6 +19,42 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <head>
+        {/* 스크립트 추가 */}
+        <script
+          defer
+          src="https://cdn.swygbro.com/public/widget/swyg-widget.js"
+        ></script>
+
+        {/* 메타 태그 추가 */}
+        <meta
+          name="description"
+          content="acTIP을 통해 SNS 콘텐츠를 분류, 정리하고 실천까지 해보세요."
+        />
+        <meta
+          name="keywords"
+          content="SNS,SNS콘텐츠,콘텐츠저장,콘텐츠보관,콘텐츠정리,플래너,액션,정보콘텐츠,꿀팁콘텐츠,콘텐츠실천,액팁,acTIPswyg,기획자,개발자,메타콘텐츠"
+        />
+        <meta property="og:site_name" content="acTIP" />
+        <meta property="og:title" content="acTIP" />
+        <meta
+          property="og:description"
+          content="acTIP을 통해 SNS 콘텐츠를 분류, 정리하고 실천까지 해보세요. "
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.acTIP.com" />
+        <meta
+          property="og:image"
+          content="https://www.acTIP.com/public/image/system/logo_color.png"
+        />
+        <meta name="twitter:title" content="acTIP" />
+
+        {/* 아이콘 추가 */}
+        {/* IOS */}
+        <link rel="apple-touch-icon" href="/image/system/logo_color.png" />
+        {/* ANDROID */}
+        <link rel="shortcut icon" href="/image/system/logo_color.png" />
+      </head>
       <body>
         <main>
           <MainPC></MainPC>
