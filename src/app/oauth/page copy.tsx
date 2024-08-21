dd8438d6f3ccae5cfcd68305c34ef1e62533618c
Suzy Kim <skme1103@naver.com>
2024-08-21 17:34:23 +0900

    test3433

--- src/app/oauth/page copy.tsx
+++ src/app/oauth/page copy.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import axios from 'axios';
 import { useEffect } from 'react';
 import DoingPage from '../my/doing/page';
 
@@ -8,25 +9,32 @@ export default function OauthMyPage() {
     const queryParams = new URLSearchParams(window.location.search);
     const code = queryParams.get('code');
     console.log('Authorization codesss:', code); // 이 줄 추가
+    
+    if (code) {
+      axios
+        .post('http://15.164.202.203:8080//api/user/login/kakao', { code })
+        .then((response) => {
+          console.log('응답 데이터:', response.data);
+          localStorage.setItem('name', response.data.user_name); // 사용자 이름 또는 토큰 저장
+        })
+        .catch((error) => {
+          console.error('카카오 로그인 중 오류 발생:', error);
+          console.log('code:', code);
 
-    const headers = {
-      "Content-Type": "application/x-www-form-urlencoded",
-    };
-
-    useEffect(() => {
-    fetch(`http://15.164.202.203:8080//api/user/login/kakao?code=${code}`, {
-      method: "POST", // 
-      headers: headers,
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        console.log(data.result.user_id);
-        console.log(data.result.jwt);
-      })
-      .catch((error) => {
-        console.error("오류 발생", error); //
-      });
+          if (error.response) {
+            console.error('서버 오류 응답 데이터:', error.response.data);
+            alert(`서버 오류: ${error.response.data.message}`);
+          } else if (error.request) {
+            console.error('서버로부터 응답이 없음:', error.request);
+            alert('서버로부터 응답을 받지 못했습니다.');
+          } else {
+            console.error('요청 설정 중 오류 발생:', error.message);
+            alert('요청 설정 중 오류가 발생했습니다.');
+          }
+        });
+    } else {
+      console.error('쿼리 파라미터에 인증 코드가 없습니다.');
+    }
   }, []);
 
   // useEffect(() => {
